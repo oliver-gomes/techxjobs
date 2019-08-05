d53fe39532672333664c3ac3a73507a29ffc80da
Oliver Gomes <ogomesphoto@gmail.com>
2019-08-05 14:27:58 -0400

    fix count

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -9,11 +9,13 @@ async function fetchGithub() {
     while (resultCount > 0) {
         const res = await fetch(`${baseUrl}?page=${onPage}`)
         const jobs = await res.json()
-        allJobs.push(jobs)
+        allJobs.push(...jobs)
+        resultCount = jobs.length
         console.log('got', jobs.length)
         onPage++
     }
-
+    console.log('total', allJobs.length)
 }
 
+fetchGithub()
 module.exports = fetchGithub;
