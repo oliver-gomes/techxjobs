48942bb5726c48d992b547825483c82aec8ae904
Oliver Gomes <ogomesphoto@gmail.com>
2019-08-05 12:24:12 -0400

    Job import

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Jobs from './components/Jobs/Jobs';
+
+const mockJobs = [
+  { title: 'SWE 1', company: 'Google' },
+  { title: 'SWE 1', company: 'Facebook' },
+  { title: 'SWE 1', company: 'Apple' },
+  { title: 'SWE 1', company: 'Uber' }
+];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jobs jobs={mockJobs} />
     </div>
   );
 }
+
 
 export default App;
