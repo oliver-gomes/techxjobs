189040bdd17bab504ce812360a50d1c35bde9651
Oliver Gomes <ogomesphoto@gmail.com>
2019-08-05 20:57:26 -0400

    implement Redis

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,21 +1,31 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
+const redis = require("redis");
 
-const baseUrl = "https://jobs.github.com/positions.json"
+const client = redis.createClient();
+
+const { promisify } = require("util");
+//const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+const baseUrl = "https://jobs.github.com/positions.json";
 
 async function fetchGithub() {
-    let resultCount = 1, onPage = 0;
-    const allJobs = []
+  let resultCount = 1,
+    onPage = 0;
+  const allJobs = [];
 
-    while (resultCount > 0) {
-        const res = await fetch(`${baseUrl}?page=${onPage}`)
-        const jobs = await res.json()
-        allJobs.push(...jobs)
-        resultCount = jobs.length
-        console.log('got', jobs.length)
-        onPage++
-    }
-    console.log('total', allJobs.length)
+  while (resultCount > 0) {
+    const res = await fetch(`${baseUrl}?page=${onPage}`);
+    const jobs = await res.json();
+    allJobs.push(...jobs);
+    resultCount = jobs.length;
+    console.log("got", jobs.length);
+    onPage++;
+  }
+  console.log("total", allJobs.length);
+  const success = await setAsync("github", JSON.stringify(allJobs));
+
+  console.log({ success });
 }
 
-fetchGithub()
 module.exports = fetchGithub;
