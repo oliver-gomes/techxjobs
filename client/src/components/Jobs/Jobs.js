e6aa80e33748bb6ef83c9d99b37d198571e309fd
Oliver Gomes <ogomesphoto@gmail.com>
2019-08-07 20:33:15 -0400

    filter implement

--- client/src/components/Jobs/Jobs.js
+++ client/src/components/Jobs/Jobs.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import SingleJob from '../SingleJob/SingleJob';
+import React from "react";
+import SingleJob from "../SingleJob/SingleJob";
 
+import { Typography } from "antd";
 
-export default function Jobs({ jobs }) {
-    console.log(jobs)
+import { Checkbox } from "antd";
+
+import "../../App.css";
+
+const { Title } = Typography;
+
+export default class Jobs extends React.Component {
+  state = {
+    checked: false,
+    reactJobs: []
+  };
+
+  checkChanged = e => {
+    this.reactFilter();
+    console.log("checked = ", e.target.checked);
+    this.setState({
+      checked: e.target.checked
+    });
+  };
+
+  reactFilter = () => {
+    const reactJobs = this.props.jobs.filter(job => {
+      const jobDesc = job.description.toLowerCase();
+
+      if (jobDesc.includes("react")) {
+        return true;
+      }
+      return false;
+    });
+
+    console.log("filtered", reactJobs.length);
+    this.setState({ reactJobs: [...reactJobs] });
+  };
+
+  render() {
+    console.log("length Original", this.props.jobs.length);
     return (
-        <div className="jobs">
-            <Typography variant="h1">Entry Level Software Jobs</Typography>
-            {jobs.map(
-                (job, i) =>
-                    <SingleJob key={i} job={job} />
-            )}
-        </div>
-    )
+      <div className="jobs">
+        <Title>Entry Level Software Jobs </Title>
+        <Checkbox onChange={this.checkChanged}>React</Checkbox>
+        {this.state.checked
+          ? this.state.reactJobs.map((job, i) => (
+              <SingleJob key={i} job={job} />
+            ))
+          : this.props.jobs.map((job, i) => <SingleJob key={i} job={job} />)}
+      </div>
+    );
+  }
 }
