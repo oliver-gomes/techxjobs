80e0ea499f081abe8c8f0f824931bb785d8a66b1
Oliver Gomes <ogomesphoto@gmail.com>
2019-08-07 20:30:27 -0400

    cors implement

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3001;
+
+app.use(cors());
 
 const redis = require("redis");
 const client = redis.createClient();
@@ -8,9 +11,9 @@ const client = redis.createClient();
 const { promisify } = require("util");
 const getAsync = promisify(client.get).bind(client);
 
-app.get("/", async (req, res) => {
+app.get("/jobs", async (req, res) => {
   const jobs = await getAsync("github");
-
+  res.header("Access-Control-Allow-Headers", "http://localhost:3000");
   return res.send(jobs);
 });
 
